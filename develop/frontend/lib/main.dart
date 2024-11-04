7c85bdc07ec274ff8632263f80f8825889dd1a0f
lud <114997596+ludovic4g@users.noreply.github.com>
2024-11-04 20:35:45 +0100

    Update splash screen, homepage

--- develop/frontend/lib/main.dart
+++ develop/frontend/lib/main.dart
@@ -11,11 +11,17 @@ Future<void> main() async {
   // Carica le variabili d’ambiente dal file .env
   await dotenv.load(fileName: 'assets/config.env');
 
-  // Imposta la modalità a schermo intero
+  // Imposta la modalità a schermo intero e il colore della status bar a bianco
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.white, // Colore bianco per la status bar
+    statusBarBrightness: Brightness.light, // Imposta il contenuto della status bar chiaro
+    statusBarIconBrightness: Brightness.dark, // Imposta le icone della status bar in colore scuro
+  ));
 
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   Future<void> _initializeSupabase() async {
@@ -47,6 +53,10 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.blue,
           ),
           home: SplashScreen(),
+          routes: {
+            '/homepage': (context) => HomePage(),
+            '/map': (context) => MapScreen(),
+          },
           debugShowCheckedModeBanner: false,
         );
       },
@@ -54,7 +64,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// SplashScreen per l'avvio dell'app con logo animato e pulsante Entra
+// SplashScreen per l'avvio dell'app con logo animato, cerchi galleggianti e pulsante Entra
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -63,28 +73,36 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
   late AnimationController _logoController;
   late AnimationController _buttonController;
-  late Animation<Offset> _logoAnimation; // Animazione di ascesa per il logo
-  late Animation<double> _buttonAnimation; // Animazione di opacità per il pulsante
+  late AnimationController _floatingCircleController;
+  late Animation<Offset> _logoAnimation;
+  late Animation<double> _logoOpacityAnimation;
+  late Animation<double> _buttonAnimation;
 
   @override
   void initState() {
     super.initState();
 
-    // Configura il controller per l'animazione di ascesa del logo
     _logoController = AnimationController(
-      duration: const Duration(seconds: 1),
+      duration: const Duration(seconds: 2),
       vsync: this,
     );
 
     _logoAnimation = Tween<Offset>(
-      begin: Offset(0, 1), // Parte da sotto lo schermo
-      end: Offset(0, 0), // Arriva alla posizione originale
+      begin: Offset(0, 0.5),
+      end: Offset(0, 0),
     ).animate(CurvedAnimation(
       parent: _logoController,
       curve: Curves.easeOut,
     ));
 
-    // Configura il controller per l'animazione del pulsante
+    _logoOpacityAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _logoController,
+      curve: Curves.easeIn,
+    ));
+
     _buttonController = AnimationController(
       duration: const Duration(seconds: 1),
       vsync: this,
@@ -95,7 +113,11 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
       curve: Curves.easeIn,
     );
 
-    // Avvia l'animazione di ascesa del logo e, al termine, mostra il pulsante "Entra"
+    _floatingCircleController = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    )..repeat(reverse: true);
+
     _logoController.forward().whenComplete(() {
       _buttonController.forward();
     });
@@ -105,92 +127,209 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
   void dispose() {
     _logoController.dispose();
     _buttonController.dispose();
+    _floatingCircleController.dispose();
     super.dispose();
+  }
+
+  Widget _buildFloatingCircle(double size, Offset offset) {
+    return Positioned(
+      left: offset.dx,
+      top: offset.dy,
+      child: AnimatedBuilder(
+        animation: _floatingCircleController,
+        builder: (context, child) {
+          return Transform.translate(
+            offset: Offset(0, 10 * (_floatingCircleController.value - 0.5)),
+            child: Container(
+              width: size,
+              height: size,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color(0xFFFFEAEA).withOpacity(0.5),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFFF6F2), // Colore di sfondo
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Logo con effetto di ascesa
-            SlideTransition(
-              position: _logoAnimation,
-              child: Container(
-                padding: EdgeInsets.all(40),
-                child: Image.asset(
-                  'assets/logo_grande.png', // Assicurati di avere un file logo_grande.png in assets
-                  width: 300,
-                  height: 300,
-                ),
-              ),
-            ),
-            SizedBox(height: 5),
-            FadeTransition(
-              opacity: _buttonAnimation,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFEB8686),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                ),
-                child: Text(
-                  "Entra",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
+      backgroundColor: Colors.white, // Impostato lo sfondo bianco
+      body: Stack(
+        children: [
+          _buildFloatingCircle(100, Offset(50, 150)),
+          _buildFloatingCircle(70, Offset(250, 250)),
+          _buildFloatingCircle(50, Offset(300, 650)),
+          
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FadeTransition(
+                  opacity: _logoOpacityAnimation,
+                  child: SlideTransition(
+                    position: _logoAnimation,
+                    child: Container(
+                      padding: EdgeInsets.all(40),
+                      child: Image.asset(
+                        'assets/logo_grande.png',
+                        width: 300,
+                        height: 300,
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                SizedBox(height: 5),
+                FadeTransition(
+                  opacity: _buttonAnimation,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/homepage');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFEB8686),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    ),
+                    child: Text(
+                      "Entra",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
 
-// HomePage con pulsanti di navigazione
+// HomePage con barra di navigazione arrotondata
+// HomePage con barra di navigazione arrotondata
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white, // Sfondo bianco
       appBar: AppBar(
-        title: Text("Homepage"),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MapScreen()),
-                );
-              },
-              child: Text("Mappa Interattiva"),
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                SizedBox(height: 50), // Spazio per abbassare il rettangolo
+                Stack(
+                  clipBehavior: Clip.none, // Permette agli elementi di uscire dai limiti
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(3), // Padding per creare lo spazio del bordo esterno
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2CFCF), // Colore del bordo esterno
+                        borderRadius: BorderRadius.circular(22), // Arrotondamento esterno
+                      ),
+                      child: Container(
+                        width: 300,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF2CFCF), // Colore del rettangolo interno
+                          borderRadius: BorderRadius.circular(20), // Arrotondamento interno
+                          border: Border.all(
+                            color: Color(0xFFEB8686), // Colore del bordo interno
+                            width: 2, // Spessore del bordo interno
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Ciao User!",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFFEB8686),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // Icona di cuore posizionata sopra il rettangolo
+                    Positioned(
+                      top: -30, // Posiziona il cuore sopra il bordo del rettangolo
+                      left: 115, // Centra l'icona rispetto al rettangolo
+                      child: Image.asset(
+                        'assets/logo_piccolo.png', // Assicurati che il file sia corretto
+                        width: 70,
+                        height: 70,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                // Navigazione alla pagina del profilo (da implementare)
-              },
-              child: Text("Profilo"),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: Container(
+                width: 300,
+                height: 70,
+                decoration: BoxDecoration(
+                  color: Color(0xFFF2CFCF),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        if (ModalRoute.of(context)?.settings.name != '/homepage') {
+                          Navigator.pushReplacementNamed(context, '/homepage');
+                        }
+                      },
+                      icon: Image.asset(
+                        'assets/home.png',
+                        width: 30,
+                        height: 30,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/map');
+                      },
+                      icon: Image.asset(
+                        'assets/mappe.png',
+                        width: 30,
+                        height: 30,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        // Implementa la navigazione per il profilo
+                      },
+                      icon: Image.asset(
+                        'assets/profile.png',
+                        width: 40,
+                        height: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
